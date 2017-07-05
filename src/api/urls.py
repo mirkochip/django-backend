c4adfb46b944d67a6cdf9852610002219d42d070
Mirko Musumeci <mirko.musumeci@kpn.com>
2017-07-05 14:31:38 +0200

    project refactored having API compliant structure

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from hello.views import HomePageView
+from api.views import HomePageView
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='home'),
--- src/main/settings.py
+++ src/main/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'hello',
+    'api.v1.hackatrain',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
